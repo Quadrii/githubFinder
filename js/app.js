d0b19d0303abe48d9e651aacde015d10bf7ab618
quadrii <adisaquadri234@yahoo.com>
2021-02-01 19:47:12 +0100

    update

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
-Github = new Github;
+const github = new Github;
+const ui = new UI;
 
 const searchUser = document.getElementById('searchUser');
 searchUser.addEventListener('keyup', (e) => {
 	const userText = e.target.value;
 
 	if(userText !==''){
-		Github.getUser(userText)
+		github.getUser(userText)
 			.then(data => {
 				if (data.profile.message === 'NOT FOUND') {
-					SHOW ALERT
+					//SHOW ALERT
+					ui.showAlert('user not found', 'alert alert-danger')
 				} else {
-					SHOW PROFILE
+					//SHOW PROFILE
+					ui.showProfile(data.profile);
+					ui.showRepos(data.repos);
 				}
 			})		
+	} else {
+		ui.clearProfile();
 	}
 })
